2aacb3be324bb9289cce039ddd3673f816f3b5db
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-29 13:41:24 +0200

    10-11

--- ejercicio10.php
+++ ejercicio10.php
@@ -10,21 +10,24 @@ try {
     $conn = new PDO("mysql:host=$servidor;dbname=$baseDatos", $usuario, $pass);
 
     $conn->beginTransaction();
-    $last_id = 0;
+    $contador = 0;
 
     // INSERTAR 3 TURISTAS DE LA MISMA FAMILIA.
     $turistas = $conn->exec("INSERT INTO turista (nombre, apellido1, apellido2, direccion, telefono) VALUES ('Antonio', 'Sanchez', 'Espinosa', 'Sevilla', '123456789')");
-    if($conn->lastInsertId() > 0) echo "Insertado con éxito.<br>";
+    $id_1 = $conn->lastInsertId();
     $turistas = $conn->exec("INSERT INTO turista (nombre, apellido1, apellido2, direccion, telefono) VALUES ('María', 'Sanchez', 'Espinosa', 'Sevilla', '987456123')");
-    if($conn->lastInsertId() > 0) echo "Insertado con éxito.<br>";
+    $id_2 = $conn->lastInsertId();
     $turistas = $conn->exec("INSERT INTO turista (nombre, apellido1, apellido2, direccion, telefono) VALUES ('Pablo', 'Sanchez', 'Espinosa', 'Sevilla', '654123789')");
-    if($conn->lastInsertId() > 0) echo "Insertado con éxito.<br>";
+    $id_3 = $conn->lastInsertId();
     
-    $conn->commit();
+    if ($id_1 > 0 && $id_2 > 0 && $id_3 > 0 && $id_1 != $id_2 && $id_1 != $id_3 &&  $id_2 != $id_3) {
+        $conn->commit();   
+    } else {
+        $conn->rollBack();
+    }
 
 } catch (PDOException $e) {
     echo "Conexión fallida: " . $e->getMessage();
-    $conn->rollBack();
 }
 
 $conn = null;
--- ejercicio11.php
+++ ejercicio11.php
@@ -1,12 +1,118 @@
 <?php
 
+try {
 
-function creaTurista() {
+    function db() {
+        $servidor = "localhost";
+        $baseDatos = "agenciaviajes";
+        $usuario = "developer";
+        $pass = "developer";
+        $conn = new PDO("mysql:host=$servidor;dbname=$baseDatos", $usuario, $pass);
 
+        return $conn;
+    }
+
+    // INSERTAR UNA NUEVA FILA.
+    function creaTurista($nombre, $apellido1, $apellido2, $direccion, $telefono) {
+        $conn = db();
+        $id = null;
+
+        $sqlInsert = "INSERT INTO turista (nombre, apellido1, apellido2, direccion, telefono) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sqlInsert);
+        $stmt->bindParam(1, $nombre);
+        $stmt->bindParam(2, $apellido1);
+        $stmt->bindParam(3, $apellido2);
+        $stmt->bindParam(4, $direccion);
+        $stmt->bindParam(5, $telefono);
+
+        $stmt->execute();
+        $last_id = $conn->lastInsertId();
+
+        $conn = null;
+
+        return $last_id;
+    }
+
+    // OBTENER UN TURISTA MEDIANTE SU ID.
+    function extraeTurista($id) {
+        $conn = db();
+        $turista = null;
+
+        $sqlQuery = "SELECT * FROM turista WHERE id = ?";
+        $stmt = $conn->prepare($sqlQuery);
+        $stmt->bindParam(1, $id);
+
+        $stmt->execute();
+
+        $turistas = $stmt->fetchAll();
+
+        $conn = null;
+
+        return $turistas;
+    }
+
+    function extraeTuristas() {
+        $conn = db();
+
+        $sqlQuery = "SELECT * FROM turista";
+        $stmt = $conn->query($sqlQuery);
+
+        $turistas = $stmt->fetchAll();
+
+        $conn = null;
+
+        return $turistas;
+    }
+
+    function actualizaTurista($id, $direccion, $telefono) {
+        $conn = db();
+        $execute = false;
+
+        $sqlUpdate = "UPDATE turista SET direccion=?,telefono=? WHERE id=?";
+        $stmt = $conn->prepare($sqlUpdate);
+        $stmt->bindParam(1, $direccion);
+        $stmt->bindParam(2, $telefono);
+        $stmt->bindParam(3, $id);
+
+        $execute = $stmt->execute();
+
+        return $execute;
+    }
+
+    function eliminaTurista($id) {
+        $conn = db();
+
+        $sqlDel = "DELETE FROM turista WHERE id=?";
+        $stmt = $conn->prepare($sqlDel);
+        $stmt->bindParam(1, $id);
+
+        $stmt->execute();
+    }
+
+} catch (PDOException $e) {
+    echo "Conexión fallida: " . $e->getMessage();
 }
 
+// CREAR TURISTA (PRUEBA).
+echo(creaTurista('Antonio', 'Sanchez', 'Espinosa', 'Sevilla', '123456789')) . "<br>";
 
+// EXTRAER TURISTA (PRUEBA).
+$turistas = extraeTurista(1);
+foreach($turistas as $turista) {
+    echo "Nombre: " . $turista['nombre'] . " " . $turista['apellido1'] . " " . $turista['apellido2'] . " Dirección: " . $turista['direccion'] . " Teléfono: " . $turista['telefono'] . "<br>";
+}
 
+// EXTREAEMOS TURISTA (PRUEBA).
+$turistas = extraeTuristas();
 
+foreach($turistas as $turista) {
+    echo "Nombre: " . $turista['nombre'] . " " . $turista['apellido1'] . " " . $turista['apellido2'] . " Dirección: " . $turista['direccion'] . " Teléfono: " . $turista['telefono'] . "<br>";
+}
+
+// ACTUALIZAMOS TURISTA (PRUEBA)(ID, DIRECCION, TELEFONO).
+echo(var_export(actualizaTurista(1,'Sevilla','654123789')));
+
+// ELIMINAR UN TURISTA (PRUEBA).
+eliminaTurista(45);
 
 ?>
